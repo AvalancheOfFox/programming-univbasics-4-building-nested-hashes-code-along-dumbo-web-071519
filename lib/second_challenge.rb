9273e62b10cb47dd092f0379e5635a7533e0c568
Ross Perkel <rossperkel@gmail.com>
2019-07-05 14:57:52 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,6 +2,9 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :patriarch => {},
       }, 
     :capulet => {
       #your key/value pairs here!
